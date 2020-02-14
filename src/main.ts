8892cb148f0cc6968844e037c40f36823b883919
Jose Castro <drakonc@gmail.com>
2020-02-14 07:02:08 -0500

    Eliminando Handlebars

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
-import * as express_handlebars from 'express-handlebars';
 import { AppModule } from './app.module';
-
-const ex_hbs = express_handlebars.create({
-  partialsDir: join(__dirname, '..', '/views/partials'),
-  layoutsDir: join(__dirname, '..', '/views/layouts'),
-  defaultLayout: 'main',
-  extname: '.hbs',
-});
-
 
 async function bootstrap() {
   const port = 3000;
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.engine('hbs', ex_hbs.engine);
-  app.set('view engine', 'hbs');
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(port, () => {
     console.log(`Servidor Corriendo en Puerto ${port}`);
